2382e35ba0b4b448fdfa14be1d3f8f210cd8d0cf
EmilianoRojas <inversiones.socony@gmail.com>
2020-06-14 16:33:43 -0400

    creating a middleware

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express');
 const path = require('path');
 const members = require('./Members');
 
+const logger = require('./middleware/logger');
+
 const app = express();
 
+
+//Init middleware
+app.use(logger);
+
+//get members data
 app.get('/api/members', (req,res) =>res.json(members));
 
 //set static folder
