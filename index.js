dc697bf3c4d2489dfa909b46129135de85cb9f5a
EmilianoRojas <inversiones.socony@gmail.com>
2020-06-14 16:10:38 -0400

    static folder

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req,res) => {
-  res.send('Hello World')
-
-});
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 5000;
 
