64b2f993c2ca71125958a9a2d725ec4ad21780cc
EmilianoRojas <inversiones.socony@gmail.com>
2020-06-14 20:43:31 -0400

    adding CRUD routes

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
 const express = require('express');
 const path = require('path');
-const members = require('./Members');
-const logger = require('./middleware/logger');
+
+
 const app = express();
 
 // //Init middleware
 // app.use(logger);
 
-//get all members data
-app.get('/api/members', (req,res) =>res.json(members));
-
-//get single member by id
-app.get('/api/members/:id', (req, res) =>{
-
-  const found = members.some(member => member.id === parseInt(req.params.id));
-
-  if(found){
-    res.json(members.filter(member => member.id === parseInt(req.params.id)));
-  }else {
-    res.status(400).json({ msg: `No member with the id of ${req.params.id}`})
-  }
-});
+//Body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
 
 //set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
+
+//Member API routes
+app.use('/api/members', require('./routes/api/members'));
 
 const PORT = process.env.PORT || 5000;
 
